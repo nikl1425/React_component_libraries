b75bc24e7346de9b8acaedc95efc08f1ef489739
niklas@astne.dk <niklas@astne.dk>
2022-12-04 17:01:51 +0100

    Latest

--- src/components/Fields/SearchField.tsx
+++ src/components/Fields/SearchField.tsx
@@ -1,4 +1,6 @@
 import React, {FC, useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 type Props = {
     dispatcher?: void;
@@ -9,8 +11,15 @@ export const SearchField: FC<Props> = ({placeholder, dispatcher}) => {
     const [searchInput, setSearchInput] = useState("");
     return (
         <>
-            <div className={` w-full `}>
-                <input className={`rounded-lg border-black border-2 placeholder:pl-2`} placeholder={placeholder ? placeholder : "search for something"}/>
+            <div className={` w-full rounded-lg border-black border-4 flex `}>
+                <div className=" float-left">
+                    <input type='text' className={`outline-0 m-1 pl-2 w-content font-arial`} placeholder={placeholder ? placeholder : "search for something"}/>
+                </div>
+                <div className="float-right w-full m-auto">
+                    <div className="mr-1">
+                        <FontAwesomeIcon className="text-primary float-right active:text-black" icon={faMagnifyingGlass} />
+                    </div>
+                </div>
             </div>
         </>
     )
